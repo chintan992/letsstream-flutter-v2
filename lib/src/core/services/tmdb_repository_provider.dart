6c211dd5f21de8b663bdf41ae3f8b465abf8bf03
chintan992 <chintanr21@gmail.com>
2025-08-25 00:43:19 -0600

    refactor: use SimpleCachedRepository in Riverpod provider

--- lib/src/core/services/tmdb_repository_provider.dart
+++ lib/src/core/services/tmdb_repository_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lets_stream/src/core/services/tmdb_repository.dart';
+import 'simple_cached_repository.dart';
 
-final tmdbRepositoryProvider = Provider<TmdbRepository>((ref) {
-  return TmdbRepository();
+final tmdbRepositoryProvider = Provider<SimpleCachedRepository>((ref) {
+  return SimpleCachedRepository();
 });
